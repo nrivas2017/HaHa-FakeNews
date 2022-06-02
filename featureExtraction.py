8ade332b8599f2d6f031717ae08079f08f55457d
nrivas2017 <nrivas2017@alu.uct.cl>
2022-06-01 23:30:01 -0400

    tf-idf

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,4 +1,7 @@
+from cgitb import reset
 from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def extraccionCaracteristicas(df):
@@ -10,9 +13,16 @@ def extraccionCaracteristicas(df):
     df['Topic'] =  le.fit_transform(df['Topic'])
     df['Source'] = le.fit_transform(df['Source'])
 
-    #TF-IDF  (?)
+    #TF-IDF  
 
+    vectorizer = TfidfVectorizer()
+
+    vectors = vectorizer.fit_transform(copia_df)
+    feature_names = vectorizer.get_feature_names_out()
+    dense = vectors.todense()
+    denselist = dense.tolist()
+
+    res = pd.DataFrame(denselist, columns=feature_names)
+    res.to_csv('./export/tf-idf.csv', header=True, index=True)
+    print(res)
     return df
-
-
-    
